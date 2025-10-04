6a4a2787a3ebb0caa967a46cd94ed3ec62e8270e
parkerpayne <parkerpayne0006@gmail.com>
2025-10-03 20:04:36 -0500

    player movement & camera

--- PlayerController.gd
+++ PlayerController.gd
@@ -1,6 +1,9 @@
 extends CharacterBody3D
 
 @export var speed: float = 5.0
+@export var rotation_speed: float = 10.0
+
+@onready var camera_rig = $CameraRig
 
 
 func _physics_process(delta):
@@ -16,8 +19,23 @@ func _physics_process(delta):
 		input.x += 1
 
 	input = input.normalized()
+	
+	var angle = deg_to_rad(45)
+	var rotated_input = Vector3(
+		input.x * cos(angle) - input.z * sin(angle),
+		0,
+		input.x * sin(angle) + input.z * cos(angle)
+	)
+	input = rotated_input
 	velocity.x = input.x * speed
 	velocity.z = input.z * speed
-	velocity.y = 0  # keep on the ground
+	velocity.y = 0
+
+	if input.length() > 0:
+		var target_rotation = atan2(input.x, input.z)
+		rotation.y = lerp_angle(rotation.y, target_rotation, rotation_speed * delta)
+		
+		if camera_rig:
+			camera_rig.global_rotation.y = 0
 
 	move_and_slide()
